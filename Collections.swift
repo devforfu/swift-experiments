12ec20987c3fc065e007b258f9d01ec3fd392ee9
devforfu <developer.z@outlook.com>
2019-02-04 21:50:47 +0500

    Stack

--- Collections.swift
+++ Collections.swift
@@ -1,1 +1,15 @@
-print("Swift Entry Point")
+struct Stack<T> {
+    private var storage: [T] = []
+    private var stackPointer: Int = 0
+
+    init(capacity: Int = 10) {
+        storage.reserveCapacity(capacity)
+    }
+
+    var empty: Bool {
+        return stackPointer == 0
+    }
+}
+
+let stack: Stack<Int> = Stack()
+print(stack.empty)
